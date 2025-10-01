b6a059cb29c3fe95531ae4a5d5d83c97d298f918
Cynthia Pinedo <cynthiapinedoh79@gmail.com>
2025-10-01 18:59:49 +0000

    sql-psycopg2.py tested in py

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -12,7 +12,7 @@ connection = psycopg2.connect(
 cursor = connection.cursor()
 
 # Query 1 - select all records from the "Artist" table
-cursor.execute('SELECT * FROM "Artist" LIMIT 10') # Using a sample query
+# cursor.execute('SELECT * FROM "Artist"') # Using a sample query
 
 # Query 2 - select only the "Name" column from the "Artist" table
 # cursor.execute('SELECT "Name" FROM "Artist"')
@@ -29,8 +29,11 @@ cursor.execute('SELECT * FROM "Artist" LIMIT 10') # Using a sample query
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
 cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
-# fetch the results
+# fetch the results (multiple)
 results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
 
 # close the connection
 connection.close()
